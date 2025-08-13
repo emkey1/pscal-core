12e33d54b6af4be430ce250bdde875df7d2f0259
Mike Miller <emkey1@gmail.com>
2025-08-13 07:13:44 -0700

    feat: add bytecode caching

---
+++ src/core/cache.h
@@ -0,0 +1,10 @@
+#ifndef PSCAL_CACHE_H
+#define PSCAL_CACHE_H
+
+#include <stdbool.h>
+#include "compiler/bytecode.h"
+
+bool loadBytecodeFromCache(const char* source_path, BytecodeChunk* chunk);
+void saveBytecodeToCache(const char* source_path, const BytecodeChunk* chunk);
+
+#endif // PSCAL_CACHE_H
