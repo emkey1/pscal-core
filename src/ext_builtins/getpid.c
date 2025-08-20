754ca3b050d993f5d9305b62ca5689bd729cace3
Mike Miller <emkey1@gmail.com>
2025-08-19 21:34:54 -0700

    Add pointer-based Swap builtin example and tests

--- src/ext_builtins/getpid.c
+++ src/ext_builtins/getpid.c
@@ -7,7 +7,8 @@ static Value vmBuiltinGetPid(struct VM_s* vm, int arg_count, Value* args) {
     return arg_count == 0 ? makeInt(getpid()) : makeInt(-1);
 }
 
-void registerExtendedBuiltins(void) {
+void registerGetPidBuiltin(void) {
     registerBuiltinFunction("GetPid", AST_FUNCTION_DECL, NULL);
     registerVmBuiltin("getpid", vmBuiltinGetPid);
 }
+
