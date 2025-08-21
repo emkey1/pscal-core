0299afd1015a399d135526f521d2f0c9e49d2d8e
Mike Miller <emkey1@gmail.com>
2025-08-21 12:15:08 -0700

    Add FileExists extended builtin

--- src/ext_builtins/register.c
+++ src/ext_builtins/register.c
@@ -4,10 +4,12 @@ void registerGetPidBuiltin(void);
 void registerSwapBuiltin(void);
 void registerFactorialBuiltin(void);
 void registerFibonacciBuiltin(void);
+void registerFileExistsBuiltin(void);
 
 void registerExtendedBuiltins(void) {
     registerGetPidBuiltin();
     registerSwapBuiltin();
     registerFactorialBuiltin();
     registerFibonacciBuiltin();
+    registerFileExistsBuiltin();
 }
