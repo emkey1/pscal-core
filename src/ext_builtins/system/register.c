d27ff2938bfea0d62f402289065357b83e1a16eb
Mike Miller <emkey1@gmail.com>
2025-08-25 12:58:18 -0700

    Organize extended builtins into categories

---
+++ src/ext_builtins/system/register.c
@@ -0,0 +1,11 @@
+#include "backend_ast/builtin.h"
+
+void registerFileExistsBuiltin(void);
+void registerGetPidBuiltin(void);
+void registerSwapBuiltin(void);
+
+void registerSystemBuiltins(void) {
+    registerFileExistsBuiltin();
+    registerGetPidBuiltin();
+    registerSwapBuiltin();
+}
