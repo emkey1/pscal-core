223cf448a93480bf16c6dce863016d1cdf05c0f9
Mike Miller <emkey1@gmail.com>
2025-08-25 19:48:14 -0700

    Add UpperCase regression test

--- src/backend_ast/builtin.c
+++ src/backend_ast/builtin.c
@@ -457,7 +457,9 @@ Value vmBuiltinSetlength(VM* vm, int arg_count, Value* args) {
     }
 
     if ((size_t)new_len > copy_len) {
-        memset(new_buf + copy_len, 0, (size_t)new_len - copy_len);
+        /* Fill remaining space with spaces so that strlen reflects the new length.
+           Using '\0' would terminate the string early and lead to length 0. */
+        memset(new_buf + copy_len, ' ', (size_t)new_len - copy_len);
     }
     new_buf[new_len] = '\0';
 
