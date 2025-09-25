9d6cfe841c94cd60ac97178aa1139d2257f49c9f
Mike Miller <emkey1@gmail.com>
2025-09-24 17:07:37 -0700

    Add accelerated landscape rendering builtins

--- src/ext_builtins/user/register.c
+++ src/ext_builtins/user/register.c
@@ -1,7 +1,12 @@
 #include "backend_ast/builtin.h"
 #include "ext_builtins/registry.h"
 
+void registerLandscapeBuiltins(void);
+
 void registerUserBuiltins(void) {
     extBuiltinRegisterCategory("user");
-    // add calls to user-defined builtins here
+    extBuiltinRegisterFunction("user", "LandscapeDrawTerrain");
+    extBuiltinRegisterFunction("user", "LandscapeDrawWater");
+
+    registerLandscapeBuiltins();
 }
