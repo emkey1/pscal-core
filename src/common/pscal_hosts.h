ba6ce18d55c42f2e5069576535a65f1db6a47b1b
Mike Miller <emkey1@oneopa.com>
2025-12-08 12:40:54 -0800

    o Tweaks to UI mostly

--- src/common/pscal_hosts.h
+++ src/common/pscal_hosts.h
@@ -4,10 +4,12 @@
 #include <netdb.h>
 #include <stdbool.h>
 
+#ifndef PSCAL_HOSTS_CUSTOM_IMPL
 /* Portable wrapper that consults the iOS container hosts file when available. */
 int pscalHostsGetAddrInfo(const char *node, const char *service,
                           const struct addrinfo *hints, struct addrinfo **res);
 void pscalHostsFreeAddrInfo(struct addrinfo *ai);
+#endif
 const char *pscalHostsGetContainerPath(void);
 bool pscalHostsLogEnabled(void);
 void pscalHostsSetLogEnabled(int enabled); /* -1 auto/env, 0 off, 1 on */
